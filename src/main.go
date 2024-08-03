b4214f30ff724ad7da934993eabdf5ce3fe33499
Aidan Barton <bartonaidan6@gmail.com>
2024-08-03 22:58:52 +0000

    Added SSL functionality
    Adding security headers
    
    Adjustments to appearce. JS ready for eventual gallery update
    
    Preping to build painting page

--- src/main.go
+++ src/main.go
@@ -10,35 +10,35 @@ import (
 )
 
 type pageData struct {
-	Nav bool
-  Content string
+	Nav     bool
+	Content string
 }
 
-var sitePath string;
+var sitePath string
 
 func paint(w http.ResponseWriter, r *http.Request, p *pageData) {
-  p.Content = "The paintings of Aidan H Barton."
+	p.Content = "The paintings of Aidan H Barton."
 	renderTmpl(w, "paint.html", p)
 }
 
 func photo(w http.ResponseWriter, r *http.Request, p *pageData) {
-  p.Content = "The photography of Aidan H Barton."
+	p.Content = "The photography of Aidan H Barton."
 	renderTmpl(w, "photo.html", p)
 }
 
 func about(w http.ResponseWriter, r *http.Request, p *pageData) {
-  p.Content = "Who is Aidan H Barton? Read about his interesting journy here."
+	p.Content = "Who is Aidan H Barton? Read about his interesting journy here."
 	renderTmpl(w, "about.html", p)
 }
 
 func home(w http.ResponseWriter, r *http.Request, p *pageData) {
-  p.Content = "Read about Aidan, or look at his work."
+	p.Content = "Read about Aidan, or look at his work."
 	renderTmpl(w, "home.html", p)
 }
 
 func index(w http.ResponseWriter, r *http.Request, p *pageData) {
 	p.Nav = false
-  p.Content = "The personal website of Aidan H Barton. Artist, Engineer, Philosopher."
+	p.Content = "The personal website of Aidan H Barton. Artist, Engineer, Philosopher."
 	renderTmpl(w, "index.html", p)
 }
 
@@ -58,9 +58,20 @@ func renderTmpl(w http.ResponseWriter, tmpl string, p *pageData) {
 	}
 }
 
+func addHeaders(w http.ResponseWriter) {
+	w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubdomains")
+
+	w.Header().Add("Content-Security-Policy", "default-src 'self' https://aidanhbarton.me/; object-src 'none'; form-action 'none'")
+
+	w.Header().Add("X-Frame-Options", "DENY")
+	w.Header().Add("X-Content-Type-Options", "nosniff")
+	w.Header().Add("Referrer-Policy", "strict-origin-when-cross-origin")
+}
+
 func handlerWrapper(fn func(http.ResponseWriter, *http.Request, *pageData)) http.HandlerFunc {
 	validPath := regexp.MustCompile("^/($|(home|about|photo|paint)/?$)")
 	return func(w http.ResponseWriter, r *http.Request) {
+		addHeaders(w)
 		log.Printf("\t%s: %s %s", r.RemoteAddr, r.Method, r.URL.Path)
 		m := validPath.MatchString(r.URL.Path)
 		if !m {
@@ -73,37 +84,44 @@ func handlerWrapper(fn func(http.ResponseWriter, *http.Request, *pageData)) http
 	}
 }
 
-func dirToJSON(path string) []byte {
+func dirToJSON(path string) ([]byte, error) {
 	var filesToJson []string
 	f, err := os.ReadDir(sitePath + "/" + path)
 
 	if err != nil {
 		log.Printf("Error reading %s: %v", path, err)
-		return []byte("")
+		return nil, err
 	}
 
 	for _, file := range f {
-		filesToJson = append(filesToJson, "/" + path + file.Name())
+		filesToJson = append(filesToJson, "/"+path+file.Name())
 	}
 
 	filesAsJson, err := json.Marshal(filesToJson)
 	if err != nil {
 		log.Printf("Error building Json")
-		return []byte("")
+		return nil, err
 	}
 
-	return filesAsJson
+	return filesAsJson, nil
+}
+
+func sendList(w http.ResponseWriter, path string) {
+	leJsone, err := dirToJSON(path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte(leJsone))
 }
 
 func buildListHandler() http.HandlerFunc {
-	validPath := regexp.MustCompile("^/list/(photos|mfa|venice|portraiture|copies)$")
-	photoJson := dirToJSON("static/files/portfolio/photo/") 
-	mfaJson := dirToJSON("static/files/portfolio/paint/mfa/")
-	veniceJson := dirToJSON("static/files/portfolio/paint/venice/")
-	portraitureJson := dirToJSON("static/files/portfolio/paint/portraiture/")
-	copiesJson := dirToJSON("static/files/portfolio/paint/copies/")
+	validPath := regexp.MustCompile("^/list/(photos|all|mfa|venice|portraiture|copies)$")
 
 	return func(w http.ResponseWriter, r *http.Request) {
+		addHeaders(w)
+		w.Header().Add("Content-Type", "text/plain")
+
 		log.Printf("\t%s: %s %s", r.RemoteAddr, r.Method, r.URL.Path)
 		m := validPath.FindStringSubmatch(r.URL.Path)
 		if m == nil {
@@ -113,15 +131,17 @@ func buildListHandler() http.HandlerFunc {
 
 		switch m[1] {
 		case "photos":
-			w.Write(photoJson)
+			sendList(w, "static/files/portfolio/photo/")
 		case "mfa":
-			w.Write(mfaJson)
+			sendList(w, "static/files/portfolio/paint/mfa/")
 		case "venice":
-			w.Write(veniceJson)
+			sendList(w, "static/files/portfolio/paint/venice/")
 		case "portraiture":
-			w.Write(portraitureJson)
+			sendList(w, "static/files/portfolio/paint/portraiture/")
 		case "copies":
-			w.Write(copiesJson)
+			sendList(w, "static/files/portfolio/paint/copies/")
+		case "all":
+			sendList(w, "static/files/portfolio/paint/venice/")
 		}
 
 	}
@@ -146,5 +166,5 @@ func main() {
 
 	port := ":5050"
 	log.Printf("\tServing at 127.0.0.1" + port)
-	log.Fatal(http.ListenAndServeTLS(port,"cert/go-server.crt", "cert/go-server.key", nil))
+	log.Fatal(http.ListenAndServeTLS(port, "cert/fullchain.pem", "cert/privkey.pem", nil))
 }
--- static/scripts.js
+++ static/scripts.js
@@ -1,72 +1,49 @@
-const isMobile = window.matchMedia("(max-width: 600px)").matches;
+const isMobile = window.matchMedia("(max-width: 700px)").matches;
+const toggler = (id, nameToToggle) => { document.getElementById(id).classList.toggle(nameToToggle) };
 
-// NavBar
-function toggle_side_bar() {
-  const toToggle = document.getElementById("side-bar-toggle");
-  toToggle.classList.toggle("opened");
+// sideBar
+const sideBarCtrl = document.getElementById('side-bar-control');
+if (sideBarCtrl) {
+  sideBarCtrl.addEventListener('click', (clickEvent) => {toggler('side-bar-toggle', 'opened')});
 }
-// end NavBar
-
-// Zoom
-const hideZoom = function () { 
-  document.getElementById("zoomed-image-window").style.visibility = "hidden"; 
-};
-
-const clickImage = function (img) {  
-  // set new img src
-  const is_showing = document.getElementById("zoomed-image");
-  is_showing.src = "";
-
-  window.requestAnimationFrame(() => {
-    is_showing.src = img;
-
-    // zoom window visibility update
-    const zoom_window = document.getElementById("zoomed-image-window");
-    if (zoom_window.style.visibility != "visible") {
-    zoom_window.style.visibility = "visible";
-    zoom_window.animate([
-      { transform: "translateY(150px)" },
-      { transform: "translateY(0px)" },
-    ], { duration: 250, iterations: 1});
-    zoom_window.animate([
-      { opacity: "0%" },
-      { opacity: "100%" },
-    ], { duration: 100, iterations: 1});
-    }
-  });
-} 
-// End Zoom
-
-// About Tabs
-let selected_tab = 'profile'
-function select_tab(tab) {
-  if (!tab) { 
-    return; 
-  }
-  
-  new_tab_element = document.getElementById(tab + "-tab");
-  new_text_element = document.getElementById(tab + "-text");
-  selected_tab_element = document.getElementById(selected_tab + "-tab");
-  selected_text_element = document.getElementById(selected_tab + "-text");
-  
-  if (new_tab_element && new_text_element) {
-    selected_tab_element.classList.remove('selected');
-    new_tab_element.classList.add('selected');
-    selected_text_element.style.display = "none";
-    new_text_element.style.display = "block";
-    selected_tab = tab;    
-  }
-}
-// End About Tabs
+// end sideBar
 
 // Galleries
 let galleryLoading = false;
+let loaderContinue = null;
+
+// Zoom
+const hideZoom = function () {
+  document.getElementById("zoomed-image-window").classList.remove("show");
+  document.getElementById("gallery").classList.toggle('hidden');
+  if (loaderContinue) {
+    loaderContinue();
+  }
+};
+
+const clickImage = function (img) {
+  // set new img src
+  const is_showing = document.getElementById("zoomed-image");
+  is_showing.src = "";
+  window.requestAnimationFrame(() => {
+    is_showing.src = img;
+    document.getElementById("zoomed-image-window").classList.add("show");
+    document.getElementById("gallery").classList.toggle('hidden');
+  });
+};
+
+const zoomBox = document.getElementById('zoom-exit');
+if (zoomBox) {
+  zoomBox.addEventListener('click', hideZoom);
+}
+// End Zoom
 
 const setupColumns = function() {
   return isMobile ? makeColumns(2) : makeColumns(4)
 };
 
 const makeColumns = function(id) {
+  // Returns an array of functions which insert a photo into column 'i'
   if (id <= 0) { // recursive break
     return [];
   }
@@ -74,43 +51,34 @@ const makeColumns = function(id) {
   const col = document.createElement("div");
   col.classList.add("column");
 
-  if (isMobile) {
-    col.style.flexWidth = '50%';
-    col.style.maxWidth = '50%';
-  }
-  
   document.getElementById("gallery").appendChild(col);
-  // Returns an array functions which insert a photo into column 'i'
   return [((image) => {
-      window.requestAnimationFrame(() => {
-        col.appendChild(image);
-      });
-  })].concat(
-    makeColumns(id-1)
-  );
+    window.requestAnimationFrame( () => {
+      col.appendChild(image);
+    });
+  })].concat(makeColumns(id-1));
 };
 
 const buildImageNode = function(imagePaths) {
   const img = document.createElement("img");
   img.src = imagePaths[0]; // small version
 
-  const a = document.createElement("a");
-  a.setAttribute("onclick", "clickImage('"+ imagePaths[1] +"')");
-  a.appendChild(img);
+  const div = document.createElement("a");
+  div.addEventListener("click", () => { clickImage(imagePaths[1]); });
+  div.appendChild(img);
 
   const image = document.createElement("div")
   image.classList.add("image");
-  image.appendChild(a);
+  image.appendChild(div);
   return image;
 };
 
-const buildPutImage = function (columns) {
-  // returns putImage function which place images into balanced colums
-  return ((image) => {
+const columnPicker = function (columns) {
+  const cols = document.getElementsByClassName("column");
+  return () => {
     // choose a column an put image in
     // find min height index
-    const cols = document.getElementsByClassName("column");
-    let minHeight = 1000000000;
+    let minHeight = Number.MAX_SAFE_INTEGER;
     let minIndex = 0;
     let index = 0;
     for (let col of cols) {
@@ -126,7 +94,16 @@ const buildPutImage = function (columns) {
       }
       index++;
     }
-    columns[minIndex](image);
+    return columns[minIndex];
+  };
+};
+
+const buildPutImage = function (columns) {
+  // returns putImage function which place images into colums
+  const chooseColumn = columnPicker(columns);
+  return ((image) => {
+    chooseColumn()(image);
+    // insert into image linked list
   });
 };
 
@@ -136,48 +113,49 @@ const populateGallery = function (putImage, imagesToLoad) {
     return;
   }
 
-  const image = buildImageNode([imagesToLoad.shift(), imagesToLoad.shift()]);
-  putImage(image);
+  if (document.getElementById('gallery').classList.contains('hidden')) {
+    loaderContinue = () => {
+        populateGallery(putImage, imagesToLoad)
+    };
+    return;
+  }
+
+  putImage(imagesToLoad.shift());
+
   setTimeout((() => {
-    window.requestAnimationFrame(() => {
-      populateGallery(putImage, imagesToLoad);
-    })
+    populateGallery(putImage, imagesToLoad);
   }), 100);
 };
 
 const galleryLoader = function(putImage, images) {
-
   // slice off quantity of images
+  // build nodes
   const imagesToLoad = function (quantity) {
     if (quantity <= 0) {
       return [];
     } else if (images.length <= 0) {
       return [];
     }
-    return [images.shift(), images.shift()].concat(imagesToLoad(quantity-1));
+    return [buildImageNode([images.shift(), images.shift()])].concat(imagesToLoad(quantity-1));
   };
 
   return (() => {
     galleryLoading = true;
     if (images.length <= 0) {
-      window.onscroll = null;
-    }  
+      window.onscroll = null; // disconnect from scoll event
+    }
     populateGallery(putImage, imagesToLoad(24));
   });
 };
 
-const initilizeGallery = function(endpoint) {
-  fetch(endpoint).then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  }).then((res) => {
-
+const initilizeGallery = function(images) {
     const putImage = buildPutImage(setupColumns());
-    const loader = galleryLoader(putImage, res);
+    const loader = galleryLoader(putImage, images);
+    loader(); // inital load
+
+    // hook loader to scrolling
     window.onscroll = function(e) {
-      const wHeight = window.innerHeight + Math.round(window.scrollY);      
+      const wHeight = window.innerHeight + Math.round(window.scrollY);
       if (!(wHeight >= document.body.offsetHeight - 400)) {
         return;
       }
@@ -185,13 +163,6 @@ const initilizeGallery = function(endpoint) {
         loader();
       }
     };
-
-    loader(); // inital load
-
-  }).catch(error => {
-    console.error('Error:', error);
-    // put loading image?
-  });
 };
 
 const clearGallery = function() {
@@ -202,25 +173,40 @@ const clearGallery = function() {
   }
 };
 
+const doFetch = function(endpoint) {
+  return fetch(endpoint).then((response) => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  }).catch(error => {
+    console.error('Error:', error);
+    return '';
+  });
+};
+
 // Photo page
-if (document.getElementById('photo')) {
-  document.addEventListener("DOMContentLoaded", (event) => {
-    initilizeGallery('/static/list/photos');
+if (location.pathname == '/photo/') {
+  document.addEventListener("DOMContentLoaded", async (event) => {
+    initilizeGallery(await doFetch('/list/photos'));
   });
 // end photo
 
 // Painting page
-} else if (document.getElementById('paint')) {
-  document.addEventListener("DOMContentLoaded", (event) => {
+} else if (location.pathname == '/paint/') {
+  document.addEventListener("DOMContentLoaded", async (event) => {
+        initilizeGallery(await doFetch('/list/all'));
   });
-}
-
-const selectGallery = function(name) {
-  const selected = document.getElementsByClassName("selected")[0];
-  const newSelected = document.getElementById(name+"-title");
-  selected.classList.toggle("selected");
-  newSelected.classList.toggle("selected");
-  clearGallery();
-  initilizeGallery('/static/list/' + name );
-};
 // end Painting
+
+// About page
+} else if (location.pathname == '/about/') {
+  const clickAboutToggle = function(clickEvent) {
+    if (clickEvent.target.classList.contains('selected')) {return;}
+    toggler('profile', 'selected');
+    toggler('artist-statement', 'selected');
+  };
+  document.getElementById('profile').addEventListener('click', clickAboutToggle);
+  document.getElementById('artist-statement').addEventListener('click', clickAboutToggle);
+}
+// end About
