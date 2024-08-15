840785cf996856a57e44f4fb2f93f87fd08d0404
Aidan Barton <bartonaidan6@gmail.com>
2024-08-15 03:09:38 +0000

    Updated image loading to preload images before placing them into columns

--- static/scripts.js
+++ static/scripts.js
@@ -37,9 +37,95 @@ if (zoomBox) {
   zoomBox.addEventListener('click', hideZoom);
 }
 // End Zoom
+const populateGallery = function (chooseColumn, nodeReadyQueue) {
+  if (nodeReadyQueue.awaiting == 0) { // all loaded
+    galleryLoading = false;
+    return
+  }
 
-const setupColumns = function() {
-  return isMobile ? makeColumns(2) : makeColumns(4)
+  if (document.getElementById('gallery').classList.contains('hidden')) {
+    // pause
+    loaderContinue = () => {
+        populateGallery(chooseColumn, nodeReadyQueue)
+    };
+    return
+  }
+
+  if (nodeReadyQueue.queue.length) {
+    chooseColumn(nodeReadyQueue.queue.shift());
+    nodeReadyQueue.awaiting--;
+  }
+
+  setTimeout((() => {
+    populateGallery(chooseColumn, nodeReadyQueue);
+  }), 100);
+};
+
+const findShortestColumn = function (columns) {
+  const cols = document.getElementsByClassName("column");
+  // choose a column an put image in
+  // find min height index
+  let minHeight = Number.MAX_SAFE_INTEGER;
+  let minIndex = 0;
+  let index = 0;
+  for (let col of cols) {
+    if (!col.lastChild) {
+      minIndex = index;
+      break;
+    }
+    let h = col.lastChild.offsetTop
+    h += col.lastChild.offsetHeight ? col.lastChild.offsetHeight : 380;
+    if (h < minHeight) {
+      minIndex = index;
+      minHeight = h;
+    }
+    index++;
+  }
+
+  return columns[minIndex];
+};
+
+const nodify = function(imagePaths, nodeReadyQueue) {
+  const img = document.createElement("img");
+
+  const div = document.createElement("a");
+  div.addEventListener("click", () => { clickImage(imagePaths[1]); });
+  div.appendChild(img);
+
+  const image = document.createElement("div")
+  image.classList.add("image");
+  image.appendChild(div);
+
+  img.addEventListener('load', () => nodeReadyQueue.queue.push(image));
+  img.src = imagePaths[0]; // small version
+  return image;
+};
+
+const buildLoader = function(columns, imagePaths) {
+  const nodeReadyQueue = {
+    'awaiting': 0,
+    'queue': new Array()
+  };
+  const buildImageNodes = function (quantity) {
+    if (quantity <= 0 || imagePaths.length <= 0) {
+      return new Array();
+    }
+
+    nodify([imagePaths.shift(), imagePaths.shift()], nodeReadyQueue);
+    nodeReadyQueue.awaiting++;
+    buildImageNodes(quantity-1);
+  };
+
+  const chooseColumn = (image) => findShortestColumn(columns)(image);
+
+  return (() => {
+    galleryLoading = true; // gallery is now loading N new nodes
+    if (imagePaths.length <= 0) {
+      window.onscroll = null; // disconnect from scoll event
+    }
+    buildImageNodes(24)
+    populateGallery(chooseColumn, nodeReadyQueue);
+  });
 };
 
 const makeColumns = function(id) {
@@ -51,113 +137,27 @@ const makeColumns = function(id) {
   const col = document.createElement("div");
   col.classList.add("column");
 
-  const putImgToDOM = function (image) {
+  const putImageIntoColumn = function (image) {
     window.requestAnimationFrame(() => {
       col.appendChild(image);
     });
   };
 
   document.getElementById("gallery").appendChild(col);
-  return [(image) => putImgToDOM(image)].concat(makeColumns(id-1));
+  return [(image) => putImageIntoColumn(image)].concat(makeColumns(id-1));
 };
 
-const buildImageNode = function(imagePaths) {
-  const img = document.createElement("img");
-  img.src = imagePaths[0]; // small version
-
-  const div = document.createElement("a");
-  div.addEventListener("click", () => { clickImage(imagePaths[1]); });
-  div.appendChild(img);
-
-  const image = document.createElement("div")
-  image.classList.add("image");
-  image.appendChild(div);
-  return image;
-};
-
-const columnPicker = function (columns) {
-  const cols = document.getElementsByClassName("column");
-  return () => {
-    // choose a column an put image in
-    // find min height index
-    let minHeight = Number.MAX_SAFE_INTEGER;
-    let minIndex = 0;
-    let index = 0;
-    for (let col of cols) {
-      if (!col.lastChild) {
-        minIndex = index;
-        break;
-      }
-      let h = col.lastChild.offsetTop
-      h += col.lastChild.offsetHeight ? col.lastChild.offsetHeight : 380;
-      if (h < minHeight) {
-        minIndex = index;
-        minHeight = h;
-      }
-      index++;
-    }
-    return columns[minIndex];
-  };
-};
-
-const buildPutImage = function (columns) {
-  // returns putImage function which place images into colums
-  const chooseColumn = columnPicker(columns);
-  return ((image) => {
-      chooseColumn()(image);
-  });
-};
-
-const populateGallery = function (putImage, imagesToLoad) {
-  if (imagesToLoad.length <= 0) {
-    galleryLoading = false;
-    return;
-  }
-
-  if (document.getElementById('gallery').classList.contains('hidden')) {
-    loaderContinue = () => {
-        populateGallery(putImage, imagesToLoad)
-    };
-    return;
-  }
-
-  putImage(imagesToLoad.shift());
-
-  setTimeout((() => {
-    populateGallery(putImage, imagesToLoad);
-  }), 100);
-};
-
-const galleryLoader = function(putImage, images) {
-  // slice off quantity of images
-  // build nodes
-  const imagesToLoad = function (quantity) {
-    if (quantity <= 0) {
-      return [];
-    } else if (images.length <= 0) {
-      return [];
-    }
-    return [buildImageNode([images.shift(), images.shift()])].concat(imagesToLoad(quantity-1));
-  };
-
-  return (() => {
-    galleryLoading = true;
-    if (images.length <= 0) {
-      window.onscroll = null; // disconnect from scoll event
-    }
-    populateGallery(putImage, imagesToLoad(24));
-  });
+const setupColumns = function() {
+  return isMobile ? makeColumns(2) : makeColumns(4)
 };
 
 const initilizeGallery = function(images) {
-    const putImage = buildPutImage(setupColumns());
-    const loader = galleryLoader(putImage, images);
+    const loader = buildLoader(setupColumns(), images);
     loader(); // inital load
-
     // hook loader to scrolling
     window.onscroll = function(e) {
-      const wHeight = window.innerHeight + Math.round(window.scrollY);
-      if (!(wHeight >= document.body.offsetHeight - 400)) {
+      const scrollLocation = window.innerHeight + Math.round(window.scrollY);
+      if (!(scrollLocation >= document.body.offsetHeight - 400)) {
         return;
       }
       if (!galleryLoading) {
@@ -183,6 +183,7 @@ const doFetch = function(endpoint) {
     return response.json();
   }).catch(error => {
     console.error('Error:', error);
+    // ERROR!! need to handle this! Redirect to a default error page
     return '';
   });
 };
